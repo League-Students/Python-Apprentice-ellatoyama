47796689d0f0e290a219d06f3e6921aeb287844c
League Student <student@jointheleague.org>
2026-07-02 13:32:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/60_More_Turtle_Programs/20_More_Turtle_Programs.py
+++ lessons/10_Turtles/60_More_Turtle_Programs/20_More_Turtle_Programs.py
@@ -6,3 +6,25 @@ Then change the code so that the turtle has a different image ( look in the 'ima
 directory ) and moves to the corners of the screen in a square pattern.
 """
 
+```python
+# Double-click to copy!
+
+import turtle                               # Import the turtle module
+
+screen = turtle.Screen()                # Set up the screen
+screen.setup(width=600, height=600)     # Set the size of the window
+screen.bgcolor('white')                 # Set the background color
+
+t1 = turtle.Turtle()                    # Create the first turtle
+t1.penup()                              # Lift the pen to move without drawing
+t1.shape("turtle")                      # Set the shape of the turtle
+
+t2 = turtle.Turtle()                    # Create the second turtle
+t2.penup()                              # Lift the pen to move without drawing
+t2.shape("arrow")                       # Set the shape of the turtle
+
+# Move both turtles in a loop
+for i in range(-200, 200):
+    t1.goto(i, i)
+    t2.goto(i, -i)
+```
