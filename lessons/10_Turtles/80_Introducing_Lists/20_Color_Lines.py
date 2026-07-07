40c0d898a1a9371f93914fccd4e26e7a3151591d
League Student <student@jointheleague.org>
2026-07-07 12:55:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/80_Introducing_Lists/20_Color_Lines.py
+++ lessons/10_Turtles/80_Introducing_Lists/20_Color_Lines.py
@@ -15,7 +15,9 @@ tina.speed(2)                           # Move at a moderate speed, not too fast
 colors = [ 'red', 'blue', 'black', 'orange']    # define a list of colors
 
 for color in colors:                            # loop through the colors
-    ... # Your code here
+    tina.color(color)
+    tina.forward(20)
+    tina.left(90)
 
 # 2) Make another square, but put the colors in reverse order, using a negative index. 
 
