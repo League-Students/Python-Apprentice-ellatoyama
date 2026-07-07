ff576d8f6fa8f7738a2b73c50e2ed58f23161006
League Student <student@jointheleague.org>
2026-07-07 12:57:14 -0700

    Automated commit

--- lessons/10_Turtles/80_Introducing_Lists/20_Color_Lines.py
+++ lessons/10_Turtles/80_Introducing_Lists/20_Color_Lines.py
@@ -22,7 +22,7 @@ for color in colors:                            # loop through the colors
 tina.forward 20
 # 2) Make another square, but put the colors in reverse order, using a negative index. 
 colorss = -[colors]
-for color in colors:
+for color in colorss:
     tina.color(color)
     tina.forward(20
     tina.left(90))
