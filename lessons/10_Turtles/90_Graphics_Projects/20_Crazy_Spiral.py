7bfb843d2430f90ecbd562bff5032418829eae25
League Student <student@jointheleague.org>
2026-07-07 13:13:21 -0700

    Automated commit

--- lessons/10_Turtles/90_Graphics_Projects/20_Crazy_Spiral.py
+++ lessons/10_Turtles/90_Graphics_Projects/20_Crazy_Spiral.py
@@ -38,14 +38,7 @@ tina.speed(0)
 # For instance, you can make it go left 30 degrees, then forward 50 pixels, 
 # then right 60 degrees, then forward 100 pixels. Make any shape you like.
 
-def make_a_shape(tina):
-    """Make a shape with turtle t. Make it go left or right or
-# 2) Call make_a_shape() in a loop to make the turtle draw a spiral.
-# For instance, you can call make_a_shape() 100 times to make a spiral with 100 shapes.
-# The second ... in the for loop should be the number of shapes you want to make,
-# for example 100, or a list of numbers.
 
-num_shapes = ...
 
 for i in range(500):
     tina.pencolor(get_random_color())
