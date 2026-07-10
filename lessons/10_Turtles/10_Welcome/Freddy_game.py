b3d68d10ac917400a743f303af0d5219fea201b8
League Student <student@jointheleague.org>
2026-07-10 12:41:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/10_Welcome/Freddy_game.py
+++ lessons/10_Turtles/10_Welcome/Freddy_game.py
@@ -7,9 +7,12 @@ Screen.setup(600,600)
 cam_colors = ['red','black','white','blue','green']
 
 def open_cam_1(1):
-    print('cam')
+    print("cam 1 open")
+    rina.
+
+
 
 screen.listen()
-screen.onekey(open_cam_1, '1')
+screen.onekey(open_cam_1, "1")
 
 turtle.exitonclick()
