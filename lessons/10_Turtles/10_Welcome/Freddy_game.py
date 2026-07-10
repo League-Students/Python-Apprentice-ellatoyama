adbe8214404e7a2b311ddd03aa200179c8e8c3e1
League Student <student@jointheleague.org>
2026-07-10 13:02:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/10_Welcome/Freddy_game.py
+++ lessons/10_Turtles/10_Welcome/Freddy_game.py
@@ -11,7 +11,10 @@ screen.setup(600,600)
 cam_colors = ['red','black','white','blue','green']
 
 def show_animatronics(cam_num):
-    if(cam_num == tina_path(tina_progress)
+    if(cam_num == tina_path(tina_progress)):
+        tina.showturtle()
+    else:
+        tina.hideturtle
 
 def open_cam_1():
     print("cam 1 open")
