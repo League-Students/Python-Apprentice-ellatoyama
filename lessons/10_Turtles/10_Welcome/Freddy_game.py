13b0ba8a877f0529f14c1d96b070d496f140e8b5
League Student <student@jointheleague.org>
2026-07-10 13:01:06 -0700

    Automated commit

--- lessons/10_Turtles/10_Welcome/Freddy_game.py
+++ lessons/10_Turtles/10_Welcome/Freddy_game.py
@@ -11,7 +11,7 @@ screen.setup(600,600)
 cam_colors = ['red','black','white','blue','green']
 
 def show_animatronics(cam_num):
-    if(cam_num == tina_path(tina_progress0)
+    if(cam_num == tina_path(tina_progress)
 
 def open_cam_1():
     print("cam 1 open")
